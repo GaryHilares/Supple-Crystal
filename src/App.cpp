bbd360b0bad5c740b0ef0d5fa319f9ab16433ad3
GaryNLOL <garypastel@gmail.com>
2021-05-06 21:39:41 -0500

    Changed resizing style

--- src/App.cpp
+++ src/App.cpp
@@ -43,6 +43,7 @@ int App::SuppleCrystal::run(int argc, char* argv[])
     std::optional<sf::Vector2i> last_clicked_mouse_position;
     while(window.isOpen())
     {
+        /* Event handling: Closing window and context menu */
         sf::Event event;
         while(window.pollEvent(event))
         {
@@ -51,28 +52,35 @@ int App::SuppleCrystal::run(int argc, char* argv[])
             case sf::Event::Closed:
                 window.close();
                 break;
+            case sf::Event::Resized:
+                {
+                    sf::View view(sf::FloatRect(0, 0, event.size.width, event.size.height));
+                    window.setView(view);
+                }
             case sf::Event::MouseButtonReleased:
                 {
                     sf::Vector2f coords = window.mapPixelToCoords({event.mouseButton.x,event.mouseButton.y});
                     MenuInterface* tmp = context_menu.containsPoint(coords);
                     if(tmp)
                         tmp->processEvent(event);
-                    else if(event.mouseButton.button == sf::Mouse::Left)
-                        context_menu.hide();
+                    else if(event.mouseButton.button == sf::Mouse::Right)
+                    {
+                        sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
+                        sf::Vector2f coords = window.mapPixelToCoords(mouse_pos);
+                        context_menu.setPosition(coords);
+                        context_menu.display();
+                    }
+                    else
+                    {
+                         context_menu.hide();
+                    }
                 }
                 break;
             default:
                 break;
             }
         }
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Right))
-        {
-            sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
-            sf::Vector2f coords = window.mapPixelToCoords(mouse_pos);
-            context_menu.setPosition(coords);
-            context_menu.display();
-
-        }
+        /* Slide */
         if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
         {
             if(last_clicked_mouse_position.has_value())
@@ -84,6 +92,7 @@ int App::SuppleCrystal::run(int argc, char* argv[])
         }
         else
             last_clicked_mouse_position.reset();
+        /* Display */
         window.clear(sf::Color::White);
         window.draw(imageDisplay);
         window.draw(context_menu);
