b78d42531bddc9be40317f37cbb757f5a9780b0f
GaryNLOL <garypastel@gmail.com>
2021-10-02 21:51:06 -0500

    Refactored code

--- include/Menus/ContextMenu.hpp
+++ include/Menus/ContextMenu.hpp
@@ -3,14 +3,4 @@
 #include <SFML/Window/Keyboard.hpp>
 #include <unordered_map>
 
-class ContextMenu
-{
-private:
-    PopupMenu* on_activate;
-    std::unordered_map<sf::Keyboard::Key,std::function<void()>> binded_keys;
-public:
-    ContextMenu();
-    void bindContextMenu(PopupMenu* new_on_activate);
-    void bindHotKey(sf::Keyboard::Key key_code, const std::function<void()>& function_to_call);
-    void processEvent(sf::RenderWindow& window, sf::Event event);
-};
+
--- src/App.cpp
+++ src/App.cpp
@@ -6,21 +6,86 @@
 #include <iostream>
 #include <windows.h>
 #include <optional>
+#include <cassert>
+
+void preventWindowContentResize(sf::RenderWindow& window, sf::Event resize_event)
+{
+    assert(resize_event.type == sf::Event::Resized);
+    sf::View view(sf::FloatRect(0, 0, resize_event.size.width, resize_event.size.height));
+    window.setView(view);
+}
+
+class HotkeysController
+{
+private:
+    std::unordered_map<sf::Keyboard::Key,std::function<void()>> hotkey_bindings;
+public:
+    HotkeysController(std::unordered_map<sf::Keyboard::Key,std::function<void()>> new_hotkey_bindings): hotkey_bindings(new_hotkey_bindings){}
+    void checkForHotkeys(sf::Event key_released_event)
+    {
+        assert(key_released_event.type == sf::Event::KeyReleased);
+        if(hotkey_bindings.find(key_released_event.key.code) != hotkey_bindings.end())
+            hotkey_bindings[key_released_event.key.code]();
+    }
+};
+
+class SlideController
+{
+private:
+    bool enabled;
+    std::optional<sf::Vector2i> last_clicked_mouse_position;
+public:
+    SlideController(): enabled(true) {}
+    void setEnabled(const bool new_enabled){enabled = new_enabled;}
+    void updateIfEnabled(const sf::RenderWindow& window, ImageDisplay& image_display)
+    {
+        if(this->enabled)
+        {
+            if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                if(this->last_clicked_mouse_position.has_value())
+                {
+                    const sf::Vector2i current_mouse_position = sf::Mouse::getPosition(window);
+                    image_display.move(-(this->last_clicked_mouse_position.value().x-current_mouse_position.x),-(this->last_clicked_mouse_position.value().y-current_mouse_position.y));
+                }
+                this->last_clicked_mouse_position = sf::Mouse::getPosition(window);
+            }
+            else
+                this->last_clicked_mouse_position.reset();
+        }
+    }
+};
+
+void processContextMenu(const sf::RenderWindow& reference_window, PopupMenu& context_menu, sf::Event mouse_event)
+{
+    assert(mouse_event.type == sf::Event::MouseButtonReleased);
+    const sf::Vector2f coords = reference_window.mapPixelToCoords({mouse_event.mouseButton.x,mouse_event.mouseButton.y});
+    if(mouse_event.mouseButton.button == sf::Mouse::Right)
+    {
+        context_menu.setPosition(coords);
+        context_menu.display();
+    }
+    else if(!context_menu.containsPoint(coords))
+    {
+        context_menu.hide();
+    }
+}
 
 int App::SuppleCrystal::run(int argc, char* argv[])
 {
+    //Load fonts.
     sf::Font arialbd;
-    if(!arialbd.loadFromFile("C:/Users/Administrator/Desktop/Supple-Crystal 0.1-alfa/calibri.ttf"))
-    {
-        std::cerr << "Couldn't open calibri.ttf!";
-        return 1;
-    }
+    assert(arialbd.loadFromFile("C:/Users/Administrator/Desktop/Supple-Crystal 0.1.0.1-alfa/calibri.ttf"));
+    //Gets the filename.
     std::string filename = argv[1];
+    //Opens and sets the window.
     sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().height),"Supple Crystal");
     ShowWindow(window.getSystemHandle(), SW_MAXIMIZE);
+    //Loads and sets the image.
     ImageDisplay imageDisplay(filename);
     imageDisplay.setPosition(sf::VideoMode::getDesktopMode().width/2,sf::VideoMode::getDesktopMode().height/2);
     imageDisplay.setOrigin(imageDisplay.getDimensions().x/2,imageDisplay.getDimensions().y/2);
+    //Creates the context_menu.
     PopupMenu context_menu({/*PopupMenuButton("Full Mode",arialbd,[&](){std::cout<<"Full mode!";}),*/
                             PopupMenuButton("Rotate left",arialbd,
                                             [&](){
@@ -39,8 +104,13 @@ int App::SuppleCrystal::run(int argc, char* argv[])
                                                 imageDisplay.scale({0.85,0.85});
                                             })
                            });
-    std::optional<sf::Vector2i> last_clicked_mouse_position;
-    bool focused = true;
+    HotkeysController hotkeys_controller({
+                                            {sf::Keyboard::Add,[&](){imageDisplay.scale(1.15,1.15);}},
+                                            {sf::Keyboard::Subtract,[&](){imageDisplay.scale(0.85,0.85);}},
+                                            {sf::Keyboard::Left,[&](){imageDisplay.rotate(90);}},
+                                            {sf::Keyboard::Right,[&](){imageDisplay.rotate(-90);}}
+                                         });
+    SlideController slide_controller;
     while(window.isOpen())
     {
         /* Event handling: Closing window and context menu */
@@ -50,67 +120,32 @@ int App::SuppleCrystal::run(int argc, char* argv[])
             switch(event.type)
             {
             case sf::Event::LostFocus:
-                focused = false;
+                slide_controller.setEnabled(false);
                 break;
             case sf::Event::GainedFocus:
-                focused = true;
+                slide_controller.setEnabled(true);
                 break;
             case sf::Event::Closed:
                 window.close();
                 break;
             case sf::Event::Resized:
-                {
-                    sf::View view(sf::FloatRect(0, 0, event.size.width, event.size.height));
-                    window.setView(view);
-                }
+                preventWindowContentResize(window,event);
+                break;
             case sf::Event::MouseButtonReleased:
                 {
-                    sf::Vector2f coords = window.mapPixelToCoords({event.mouseButton.x,event.mouseButton.y});
-                    MenuInterface* tmp = context_menu.containsPoint(coords);
-                    if(tmp)
-                        tmp->processEvent(event);
-                    else if(event.mouseButton.button == sf::Mouse::Right)
-                    {
-                        sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
-                        sf::Vector2f coords = window.mapPixelToCoords(mouse_pos);
-                        context_menu.setPosition(coords);
-                        context_menu.display();
-                    }
-                    else
-                    {
-                         context_menu.hide();
-                    }
+                    const sf::Vector2f coords = window.mapPixelToCoords({event.mouseButton.x,event.mouseButton.y});
+                    MenuInterface* menu_in_coords = context_menu.containsPoint(coords);
+                    if(menu_in_coords) menu_in_coords->processEvent(event);
+                    processContextMenu(window,context_menu,event);
                 }
                 break;
             case sf::Event::KeyReleased:
-                /* Hotkeys */
-                if(event.key.code == sf::Keyboard::Add)
-                    imageDisplay.scale(1.15,1.15);
-                else if(event.key.code == sf::Keyboard::Subtract)
-                    imageDisplay.scale(0.85,0.85);
-                else if(event.key.code == sf::Keyboard::Left)
-                    imageDisplay.rotate(90);
-                else if(event.key.code == sf::Keyboard::Right)
-                    imageDisplay.rotate(-90);
+                hotkeys_controller.checkForHotkeys(event);
             default:
                 break;
             }
         }
-        /* Slide */
-        if(focused)
-        {
-            if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
-            {
-                if(last_clicked_mouse_position.has_value())
-                {
-                    const sf::Vector2i current_mouse_position = sf::Mouse::getPosition(window);
-                    imageDisplay.move(-(last_clicked_mouse_position.value().x-current_mouse_position.x),-(last_clicked_mouse_position.value().y-current_mouse_position.y));
-                }
-                last_clicked_mouse_position = sf::Mouse::getPosition(window);
-            }
-            else
-                last_clicked_mouse_position.reset();
-        }
+        slide_controller.updateIfEnabled(window,imageDisplay);
         /* Display */
         window.clear(sf::Color::White);
         window.draw(imageDisplay);
