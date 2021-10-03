b031220563b583d0a294815d822b7e7b66f78e9e
GaryNLOL <garypastel@gmail.com>
2021-10-02 22:40:35 -0500

    Corrected bug when compiling with GCC

--- src/App.cpp
+++ src/App.cpp
@@ -36,7 +36,9 @@ private:
     bool enabled;
     std::optional<sf::Vector2i> last_clicked_mouse_position;
 public:
-    SlideController(): enabled(true) {}
+    // std::make_optional may see pretty redundant, but is to avoid bugs in GCC.
+    // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80635
+    SlideController(): enabled(true), last_clicked_mouse_position(std::make_optional<sf::Vector2i>()) {}
     void setEnabled(const bool new_enabled){enabled = new_enabled;}
     void updateIfEnabled(const sf::RenderWindow& window, ImageDisplay& image_display)
     {
@@ -47,7 +49,8 @@ public:
                 if(this->last_clicked_mouse_position.has_value())
                 {
                     const sf::Vector2i current_mouse_position = sf::Mouse::getPosition(window);
-                    image_display.move(-(this->last_clicked_mouse_position.value().x-current_mouse_position.x),-(this->last_clicked_mouse_position.value().y-current_mouse_position.y));
+                    image_display.move(-(this->last_clicked_mouse_position.value().x-current_mouse_position.x),
+                                       -(this->last_clicked_mouse_position.value().y-current_mouse_position.y));
                 }
                 this->last_clicked_mouse_position = sf::Mouse::getPosition(window);
             }
