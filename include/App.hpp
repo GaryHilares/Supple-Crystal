607e818f2e2616e0d590a97424c86be005230669
GaryNLOL <garypastel@gmail.com>
2021-11-13 14:30:29 -0500

    Created polished mode interface

--- include/App.hpp
+++ include/App.hpp
@@ -5,6 +5,14 @@ namespace App
     class SuppleCrystal
     {
     public:
+        enum class Activity{FastMode, PolishedMode};
+    private:
+        Activity current_activity;
+        int exit_code;
+        bool do_exit;
+        void runFastMode(const int argc, char* argv[]);
+        void runPolishedMode(const int argc, char* argv[]);
+    public:
         int run(const int argc, char* argv[]);
     };
 }
