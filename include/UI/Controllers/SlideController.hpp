e01fe44ffc17fc64dab33e2a4886aae3e8d56ef7
GaryNLOL <dev.garynlol@gmail.com>
2021-11-20 12:19:42 -0500

    Updated SlideController documentation.

--- include/UI/Controllers/SlideController.hpp
+++ include/UI/Controllers/SlideController.hpp
@@ -11,6 +11,11 @@ private:
     std::optional<sf::Vector2i> last_clicked_mouse_position;
     ImageDisplay& image_display;
 public:
+    /**
+     * @brief Construct a new SlideController object
+     * 
+     * @param new_image_display A reference to the image to control.
+     */
     SlideController(ImageDisplay& new_image_display);
     /**
      * @brief Set whether the SlideController is enabled or not.
@@ -18,5 +23,10 @@ public:
      * @param new_enabled New enabled value.
      */
     void setEnabled(const bool new_enabled);
+    /**
+     * @brief Check for MouseMoved events and slides the image accordingly.
+     * 
+     * @param event Event to check for updates.
+     */
     void checkForUpdates(sf::Event event) override;
 };
