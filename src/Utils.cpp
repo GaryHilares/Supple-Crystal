1c2554c1b82523de8a5b186ebe1d6715fecb3224
GaryNLOL <dev.garynlol@gmail.com>
2021-11-21 21:34:07 -0500

    Added "jfif" file extension

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -18,5 +18,5 @@ bool isSupportedImageType(std::experimental::filesystem::path file)
 {
     std::string extension = file.string().substr(file.string().find_last_of(".")+1,file.string().size());
     std::transform(extension.begin(), extension.end(), extension.begin(), [](unsigned char c){ return std::tolower((unsigned int) c);});
-    return in<std::string>(extension,{"bmp","png","tga","jpg","jpeg","gif","psd","hdr","pic"});
+    return in<std::string>(extension,{"bmp","png","tga","jpg","jpeg","jfif","gif","psd","hdr","pic"});
 }
