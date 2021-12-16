5aa56b4fd102b8420eb9670f512fc5ddde76d39c
GaryNLOL <dev.garynlol@gmail.com>
2021-12-16 12:10:26 -0500

    Copied FastMode implementation to PolishedMode

--- src/ImageViewer.cpp
+++ src/ImageViewer.cpp
@@ -33,7 +33,7 @@ void ImageViewer::runFastMode()
     this->status = ImageViewerStatus::OngoingTask;
     const std::experimental::filesystem::path file_path = this->files.cur();
     sf::Font font = loadFromFileWithFallbacks<sf::Font>("PublicSans-Regular.ttf",{this->resource_folder.string() + "/","","C:/Users/Administrator/Desktop/Supple-Crystal 0.1.0.2-alfa/"});
-    sf::Image icon = loadFromFileWithFallbacks<sf::Image>("assets/logo_bg-true_resized.png",{resource_folder.string() + "/","","C:/Users/Administrator/Desktop/Supple-Crystal 0.1.0.2-alfa/"});
+    sf::Image icon = loadFromFileWithFallbacks<sf::Image>("assets/logo_bg-true_resized.png",{this->resource_folder.string() + "/","","C:/Users/Administrator/Desktop/Supple-Crystal 0.1.0.2-alfa/"});
     const std::string& starting_window_title_utf8 = file_path.u8string().substr(file_path.u8string().find_last_of("/\\")+1,file_path.u8string().size()) + " - Supple Crystal: Fast mode";
     sf::String starting_window_title = sf::String::fromUtf8(starting_window_title_utf8.begin(),starting_window_title_utf8.end());
     sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().height),starting_window_title);
@@ -116,7 +116,7 @@ void ImageViewer::runFastMode()
         }
         /* Display */
         window.clear(sf::Color(Constants::background_color));
-        window.draw(image_display);
+        window.draw(this->image_display);
         window.draw(context_menu);
         window.display();
     }
@@ -125,7 +125,95 @@ void ImageViewer::runFastMode()
 void ImageViewer::runPolishedMode()
 {
     this->status = ImageViewerStatus::OngoingTask;
-    this->status = ImageViewerStatus::Error;
+    const std::experimental::filesystem::path file_path = this->files.cur();
+    sf::Font font = loadFromFileWithFallbacks<sf::Font>("PublicSans-Regular.ttf",{this->resource_folder.string() + "/","","C:/Users/Administrator/Desktop/Supple-Crystal 0.1.0.2-alfa/"});
+    sf::Image icon = loadFromFileWithFallbacks<sf::Image>("assets/logo_bg-true_resized.png",{this->resource_folder.string() + "/","","C:/Users/Administrator/Desktop/Supple-Crystal 0.1.0.2-alfa/"});
+    const std::string& starting_window_title_utf8 = file_path.u8string().substr(file_path.u8string().find_last_of("/\\")+1,file_path.u8string().size()) + " - Supple Crystal: Polished mode";
+    sf::String starting_window_title = sf::String::fromUtf8(starting_window_title_utf8.begin(),starting_window_title_utf8.end());
+    sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().height),starting_window_title);
+    window.setFramerateLimit(60);
+    window.setIcon(32,32,icon.getPixelsPtr());
+    OS::maximizeWindow(window);
+    auto run_polished_mode = [&](){this->status = ImageViewerStatus::RunPolishedMode;};
+    auto rotate_image_to_the_right = [&](){
+        this->image_display.rotate(90);
+    };
+    auto rotate_image_to_the_left = [&](){
+        this->image_display.rotate(-90);
+    };
+    auto zoom_image = [&](){this->image_display.scale({1.1,1.1});};
+    auto unzoom_image = [&](){this->image_display.scale({0.8,0.8});};
+    auto set_next_image = [&](){
+        this->openImageFromPath(this->files.next());
+        const std::string& window_title_utf8 = this->files.cur().u8string().substr(this->files.cur().u8string().find_last_of("/\\")+1,this->files.cur().u8string().size()) + " - Supple Crystal: Fast mode";
+        sf::String window_title = sf::String::fromUtf8(window_title_utf8.begin(),window_title_utf8.end());
+        window.setTitle(window_title);
+        std::cout << this->files.cur().string() << std::endl;
+    };
+    const auto set_previous_image = [&](){
+        this->openImageFromPath(this->files.prev());
+        const std::string& window_title_utf8 = this->files.cur().u8string().substr(this->files.cur().u8string().find_last_of("/\\")+1,this->files.cur().u8string().size()) + " - Supple Crystal: Fast mode";
+        sf::String window_title = sf::String::fromUtf8(window_title_utf8.begin(),window_title_utf8.end());
+        window.setTitle(window_title);
+        std::cout << this->files.cur().string() << std::endl;
+    };
+    ContextMenu context_menu({
+                                std::make_shared<PopupMenuButton>(PopupMenuButton("Polished Mode",font,run_polished_mode)),
+                                std::make_shared<PopupMenuButton>(PopupMenuButton("Rotate left",font,rotate_image_to_the_left)),
+                                std::make_shared<PopupMenuButton>(PopupMenuButton("Rotate right",font,rotate_image_to_the_right)),
+                                std::make_shared<PopupMenuButton>(PopupMenuButton("Increase zoom",font,zoom_image)),
+                                std::make_shared<PopupMenuButton>(PopupMenuButton("Decrease zoom",font,unzoom_image)),
+                                std::make_shared<PopupMenuButton>(PopupMenuButton("Next image",font,set_next_image)),
+                                std::make_shared<PopupMenuButton>(PopupMenuButton("Previous image",font,set_previous_image))
+                           });
+    HotkeysController hotkeys_controller({
+                                            {sf::Keyboard::Add,zoom_image},
+                                            {sf::Keyboard::Subtract,unzoom_image},
+                                            {sf::Keyboard::Left,rotate_image_to_the_left},
+                                            {sf::Keyboard::Right,rotate_image_to_the_right},
+                                            {sf::Keyboard::Up,set_next_image},
+                                            {sf::Keyboard::Down,set_previous_image}
+                                         });
+    SlideController slide_controller(this->image_display);
+    while(this->status == ImageViewerStatus::OngoingTask && window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            switch(event.type)
+            {
+            case sf::Event::LostFocus:
+                slide_controller.setEnabled(false);
+                break;
+            case sf::Event::GainedFocus:
+                slide_controller.setEnabled(true);
+                break;
+            case sf::Event::Closed:
+                window.close();
+                this->status = ImageViewerStatus::LeftOk;
+                break;
+            case sf::Event::MouseMoved:
+                slide_controller.checkForUpdates(event);
+                break;
+            case sf::Event::Resized:
+                preventWindowContentResize(window,event);
+                break;
+            case sf::Event::MouseButtonReleased:
+                context_menu.processEvent(event);
+                break;
+            case sf::Event::KeyReleased:
+                hotkeys_controller.checkForUpdates(event);
+                break;
+            default:
+                break;
+            }
+        }
+        /* Display */
+        window.clear(sf::Color(Constants::background_color));
+        window.draw(this->image_display);
+        window.draw(context_menu);
+        window.display();
+    }
 }
 
 ImageViewerStatus ImageViewer::getStatus()
