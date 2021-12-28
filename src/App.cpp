2018ca0913db60303c99878a18cca728ef785016
GaryNLOL <dev.garynlol@gmail.com>
2021-12-27 22:56:27 -0500

    Corrected std::cerr usage

--- src/App.cpp
+++ src/App.cpp
@@ -36,19 +36,17 @@ int App::run(const int argc, char* argv[])
                 this->do_exit = true;
                 break;
             case ImageViewerStatus::OngoingTask:
-                std::cerr << "OngoingTask: Something was not expected to happen" << std::endl;
-                throw;
+                throw std::runtime_error("OngoingTask: Something was not expected to happen");
             case ImageViewerStatus::NothingAssigned:
                 file_path = viewer.runFastMode(file_path).second;
                 break;
             }
         }
-        return this->exit_code;
     } catch (const std::runtime_error& error) {
+        std::cerr << error.what() << std::endl;
         if (argc >= 1) {
             const std::experimental::filesystem::path executable_path = std::string(argv[0]);
             const std::experimental::filesystem::path resource_folder = executable_path.parent_path();
-            std::cerr << error.what() << std::endl;
             std::optional<sf::Font> loading_object = loadFromFileWithFallbacks<sf::Font>("PublicSans-Regular.ttf", { resource_folder.string() + "/", "", "C:/Users/Administrator/Desktop/Supple-Crystal 0.1.0.2-alfa/" });
             if (loading_object.has_value())
                 MessageBox("Error", error.what(), loading_object.value());
@@ -57,8 +55,9 @@ int App::run(const int argc, char* argv[])
         } else {
             std::cerr << "The program received less than 1 argument." << std::endl;
         }
-        return 1;
+        this->exit_code = 1;
     }
+    return this->exit_code;
 }
 
 App::App()
